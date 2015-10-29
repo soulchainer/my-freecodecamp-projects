3b967d2e1bc79fee9897f90e0f078c911f9f3d58
soulchainer <soulchainer@gmail.com>
2015-10-29 17:44:25 +0100

    Add e˟, Shift/hyp functionality and a few fixes
    - Added e˟ function and removed E constant.
      When e˟ is called without arguments, it returns e^1.
    - Added Shift and hyp functionality. When we hit these
      buttons, we can see other functions/operators, which
      are hidden in «normal» mode.
    - Added a few  comments.
    - Deleted useless spaces from atribute names of «operators»
      object.
    - Buttons rearranged in UI.
    - Fixed parentheses functionality.
    - Fixed redundant code.

--- BasicFrontEnd/05_BuildJavaScriptCalculator/src/js/app.js
+++ BasicFrontEnd/05_BuildJavaScriptCalculator/src/js/app.js
@@ -7,26 +7,36 @@
   decimal.config({ precision: 50 });
 
   var ans;
+  var altFunctions = {
+    'EE': {'type': 'constant', 'value': 'π', 'label': 'π'},
+    'ln': {'value': 'e', 'label': 'e˟'},
+    '×': {'value': '%', 'label': '%'},
+    '^': {'value': '˟√', 'label': '˟√'},
+  };
   var operators = {
     '-': {"op": negative, "precedence": 6, "arity": 1, "associativity": "right"},
     // implied multiplication
     '·': {"op": multiply, "precedence": 5, "arity": 2, "associativity": "left"},
     'E': {"op": exponent, "precedence": 5, "arity": 2, "associativity": "left"},
+    'e': {"op": exponential, "precedence": 5, "arity": 1, "associativity": "right"},
     '%': {"op": percent, "precedence": 5, "arity": 2, "associativity": "right"},
     '^': {"op": power, "precedence": 4, "arity": 2, "associativity": "left"},
     '√': {"op": sqrt, "precedence": 4, "arity": 1, "associativity": "right"},
-    'acos ': {"op": acos, "precedence": 3, "arity": 1, "associativity": "left"},
-    'acosh ': {"op": acosh, "precedence": 3, "arity": 1, "associativity": "left"},
-    'asin ': {"op": asin, "precedence": 3, "arity": 1, "associativity": "left"},
-    'asinh ': {"op": asinh, "precedence": 3, "arity": 1, "associativity": "left"},
-    'atan ': {"op": atan, "precedence": 3, "arity": 1, "associativity": "left"},
-    'atanh ': {"op": atanh, "precedence": 3, "arity": 1, "associativity": "left"},
-    'cos ': {"op": cos, "precedence": 3, "arity": 1, "associativity": "left"},
-    'cosh ': {"op": cosh, "precedence": 3, "arity": 1, "associativity": "left"},
-    'sin ': {"op": sin, "precedence": 3, "arity": 1, "associativity": "left"},
-    'sinh ': {"op": sinh, "precedence": 3, "arity": 1, "associativity": "left"},
-    'tan ': {"op": tan, "precedence": 3, "arity": 1, "associativity": "left"},
-    'tanh ': {"op": tanh, "precedence": 3, "arity": 1, "associativity": "left"},
+    '˟√': {"op": nthRoot, "precedence": 4, "arity": 2, "associativity": "right"},
+    'acos': {"op": acos, "precedence": 3, "arity": 1, "associativity": "left"},
+    'acosh': {"op": acosh, "precedence": 3, "arity": 1, "associativity": "left"},
+    'asin': {"op": asin, "precedence": 3, "arity": 1, "associativity": "left"},
+    'asinh': {"op": asinh, "precedence": 3, "arity": 1, "associativity": "left"},
+    'atan': {"op": atan, "precedence": 3, "arity": 1, "associativity": "left"},
+    'atanh': {"op": atanh, "precedence": 3, "arity": 1, "associativity": "left"},
+    'cos': {"op": cos, "precedence": 3, "arity": 1, "associativity": "left"},
+    'cosh': {"op": cosh, "precedence": 3, "arity": 1, "associativity": "left"},
+    'log': {"op": log, "precedence": 3, "arity": 1, "associativity": "right"},
+    'ln': {"op": ln, "precedence": 3, "arity": 1, "associativity": "right"},
+    'sin': {"op": sin, "precedence": 3, "arity": 1, "associativity": "left"},
+    'sinh': {"op": sinh, "precedence": 3, "arity": 1, "associativity": "left"},
+    'tan': {"op": tan, "precedence": 3, "arity": 1, "associativity": "left"},
+    'tanh': {"op": tanh, "precedence": 3, "arity": 1, "associativity": "left"},
     '÷': {"op": divide, "precedence": 2, "arity": 2, "associativity": "left"},
     '×': {"op": multiply, "precedence": 2, "arity": 2, "associativity": "left"},
     '+': {"op": add, "precedence": 1, "arity": 2, "associativity": "left"},
@@ -36,7 +46,6 @@
 
 // Constants
   var PI = new decimal('3.14159265358979323846264338327950288419716939937510');
-  var E = decimal.exp(1);
 // Operations
   // Arithmetic
   function add(x, y) {
@@ -47,8 +56,12 @@
     return x.div(y);
   }
 
+  function exponential(x) {
+    return (x || new decimal(1)).exp();
+  }
+
   function exponent(x, y) {
-    return x.times(decimal.pow(10, y));
+    return (x || new decimal(1)).times(decimal.pow(10, y));
   }
 
   function log(x) {
@@ -87,7 +100,6 @@
     return x.minus(y);
   }
   // Trygonometry
-
   function sin(x) {
     return new decimal(String(Math.sin(x.toNumber())));
   }
@@ -104,7 +116,7 @@
     return sin(PI.div(2).plus(x));
   }
   function acos(x) {
-    return atan((x.neg()).div(x.pow(2).neg().plus(1).sqrt())).plus(atan(1).times(2));
+    return atan((x.neg()).div(x.pow(2).neg().plus(1).sqrt())).plus(atan(new decimal(1)).times(2));
   }
   function cosh(x) {
     return (E.pow(x).plus(E.pow(x.neg()))).div(2);
@@ -119,7 +131,7 @@
     return new decimal(String(Math.atan(x.toNumber())));
   }
   function tanh(x) {
-    return sinh(x).divideBy(cosh(x));
+    return sinh(x).div(cosh(x));
   }
   function atanh(x){
     return x.plus(1).div(x.neg().plus(1)).ln().times(0.5);
@@ -132,10 +144,8 @@
     var x, y;
     if (arity === 2) {
       y = outputStack.pop();
-      x = outputStack.pop();
-    } else {
-      x = outputStack.pop();
     }
+    x = outputStack.pop();
     outputStack.push(operation(x, y));
     postfix += op + " ";
   }
@@ -184,24 +194,35 @@
     ans = 0;
     $scope.expression = []; // Array with the elements of the expression
     $scope.previousButton = ''; // String with type of previous button pressed
+    $scope.shiftOn = false;
+    $scope.hypOn = false;
     $scope.clear = function() {
       $scope.expression = [];
       $scope.previousButton = '';
+    };
+    $scope.shift = function() {
+      $scope.shiftOn = !$scope.shiftOn;
+      $scope.inv = ($scope.shiftOn)? 'a': '';
+      $scope.alt = ($scope.shiftOn)? altFunctions: null;
+    };
+    $scope.hyperbolic = function() {
+      $scope.hypOn = !$scope.hypOn;
+      $scope.hyp = ($scope.hypOn)? 'h': '';
     };
     $scope.addToExpression = function(event) {
       var element = event.target;
       var type = element.dataset.type;
       var value = element.value;
       var prev = $scope.previousButton;
-      if (prev === 'operand' && type === prev) {
-        if (value === 'Ans') {
+      if (prev === 'operand' && type === prev) {  // in progress number
+        if (value === 'Ans') {  // an implicit multiplication with Ans found
           $scope.expression.push({'type': 'operator', 'value': '·'},
                                  {'type': type, 'value': value});
         } else if (!((value === '.') && ($scope.expression[$scope.expression.length -1].value.indexOf(value) !== -1)))
-        {
+        {  // No repeated dots, so add new operand to the in progress number
           $scope.expression[$scope.expression.length -1].value += value;
         }
-      } else {
+      } else {  // Not an in progress number, add the element to expression
         $scope.expression.push({'type': type, 'value': value});
       }
       $scope.previousButton = type;
@@ -210,8 +231,7 @@
     $scope.evalExpression = function() {
       ans = evaluate($scope.expression);
       $scope.clear();
-      console.log("Expression: " + ans.join(""));
-      // console.log(ans.toString());
+      console.log("Result: " + ans.join(""));
       console.log("Postfix: " + postfix);
       postfix = '';
     };
