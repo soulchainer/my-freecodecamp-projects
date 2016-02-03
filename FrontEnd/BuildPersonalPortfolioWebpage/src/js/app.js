b877744f162e0258baa43478261e8eb72e1b8aeb
soulchainer <soulchainer@gmail.com>
2016-02-03 20:06:44 +0100

    Add smooth scrolling

--- FrontEnd/BuildPersonalPortfolioWebpage/src/js/app.js
+++ FrontEnd/BuildPersonalPortfolioWebpage/src/js/app.js
@@ -1,3 +1,29 @@
+var btns = {'btn-home': document.getElementById('home'),
+            'btn-about': document.getElementById('about'),
+            'btn-projects': document.getElementById('projects'),
+            'btn-contact': document.getElementById('contact')
+            };
+function smoothScroll(time) {
+  function animate(e) {
+    // smooth scrolling from @rahul_send89 function
+    // http://stackoverflow.com/a/26094310/1405004
+    var elem = btns[e.target.id];
+    var to = elem.offsetTop;
+    var from = window.scrollY;
+    var start = new Date().getTime(),
+        timer = setInterval(function() {
+          var step = Math.min(1,(new Date().getTime()-start)/time);
+              window.scrollTo(0,(from+step*(to-from))+1);
+              if( step === 1){
+                clearInterval(timer);
+              }
+        },25);
+        window.scrollTo(0,(from+1));
+  }
+  return animate;
+}
+
+
 (function(){
   var Vivus = require('vivus');
 
@@ -10,4 +36,10 @@
     paths.classList.add('drawn');
   });
   greeting.play(1);
+
+  // smooth scrolling
+  var btns = document.querySelectorAll('.nav-link>a');
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', smoothScroll(200));
+  }
 })();
