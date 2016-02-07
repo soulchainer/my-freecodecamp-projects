7b071587266f5b618f0b81b66f5c351e10c5f753
soulchainer <soulchainer@gmail.com>
2016-02-07 12:58:52 +0100

    Fill «Contact» section of portfolio

--- FrontEnd/BuildPersonalPortfolioWebpage/src/js/app.js
+++ FrontEnd/BuildPersonalPortfolioWebpage/src/js/app.js
@@ -26,7 +26,9 @@ function smoothScroll(time) {
 
 (function(){
   var Vivus = require('vivus');
+  var L = require('leaflet');
 
+  // «¡Hola!» svg drawing animation
   var paths = document.getElementById('paths');
   var greeting = new Vivus('greeting',  {
     type: "oneByOne",
@@ -36,6 +38,23 @@ function smoothScroll(time) {
     paths.classList.add('drawn');
   });
   greeting.play(1);
+
+  // render the map of contact section
+  var map = L.map('map').setView([37.9410106, -1.1398814], 11);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoianVhbnJpcWdvbiIsImEiOiJjaWs5MGR2bTAwMDA4d2xsdjNwOXQ3eXQ1In0.Y7PbFkq-Wxa2Kn5O1CWgUg', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'juanriqgon.p2ln61p7',
+    accessToken: 'pk.eyJ1IjoianVhbnJpcWdvbiIsImEiOiJjaWs5MGR2bTAwMDA4d2xsdjNwOXQ3eXQ1In0.Y7PbFkq-Wxa2Kn5O1CWgUg'
+  }).addTo(map);
+  L.Icon.Default.imagePath = '../assets/images';
+  var marker = L.marker([37.9410106, -1.1398814]).addTo(map);
+  marker.bindPopup("I live around here.").openPopup();
+
+  // render the contact form
+  var mapDiv = document.getElementById('map');
+  var contactForm = '<form class="contact-form" action="http://pooleapp.com/stash/52a1c0eb-dcc6-45b9-8923-73309fbb729b/" method="post">\n\t<input type="hidden" name="redirect_to" value="{YOUR-THANKS-PAGE}" />\n\t<p>\n\t\t<label for="name">Name\n\t\t\t<input class="contact-field" type="text" name="name" id="name" placeholder="What\'s your name?" />\n\t\t</label>\n\t</p>\n\t<p>\n\t\t<label for="e-mail">E-mail\n\t\t\t<input class="contact-field" type="email" name="email" id="email" placeholder="An email to answer you">\n\t\t</label>\n\t</p>\n\t<p>\n\t\t<label for="message">Message\n\t\t\t<textarea class="message" id="message" name="message" rows="5" placeholder="Your message"></textarea>\n\t\t</label>\n\t</p>\n\t<p>\n\t\t<input class="submit" type="submit" value="Submit" />\n\t</p>\n</form>';
+  mapDiv.insertAdjacentHTML('afterend', contactForm);
 
   // smooth scrolling
   var btns = document.querySelectorAll('.nav-link>a');
