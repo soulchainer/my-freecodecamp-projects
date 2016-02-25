8c5c21dd3a6b3c49d21a8fbbac8d0cb304b02f37
soulchainer <soulchainer@gmail.com>
2016-02-26 00:03:52 +0100

    Simplify portfolio «Contact» section

--- FrontEnd/BuildPersonalPortfolioWebpage/src/js/app.js
+++ FrontEnd/BuildPersonalPortfolioWebpage/src/js/app.js
@@ -57,36 +57,6 @@ function debounce(func, wait, immediate) {
     $paths.addClass('drawn');
   });
 
-  function onMarkerClick(e) {
-      e.target.openPopup();
-  }
-
-  {
-    // render the map of contact section
-    let map = L.map('map', {
-      center: [37.9410106, -1.1398814],
-      doubleClickZoom: 'center',
-      dragging: false,
-      scrollWheelZoom: false,
-      zoom: 11
-    });
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoianVhbnJpcWdvbiIsImEiOiJjaWs5MGR2bTAwMDA4d2xsdjNwOXQ3eXQ1In0.Y7PbFkq-Wxa2Kn5O1CWgUg', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18,
-      id: 'juanriqgon.p2ln61p7',
-      accessToken: 'pk.eyJ1IjoianVhbnJpcWdvbiIsImEiOiJjaWs5MGR2bTAwMDA4d2xsdjNwOXQ3eXQ1In0.Y7PbFkq-Wxa2Kn5O1CWgUg'
-    }).addTo(map);
-    L.Icon.Default.imagePath = '../assets/images';
-    let marker = L.marker([37.9410106, -1.1398814]).addTo(map);
-    marker.bindPopup("I live around here.");
-    marker.addEventListener('click', onMarkerClick);
-  }
-
-  // render the contact form
-  var $contactWays = $('#contact-ways');
-  var contactForm = '<form class="contact-form" action="http://pooleapp.com/stash/52a1c0eb-dcc6-45b9-8923-73309fbb729b/" method="post">\n\t<input type="hidden" name="redirect_to" value="{YOUR-THANKS-PAGE}" />\n\t<p>\n\t\t<label for="name">Name\n\t\t\t<input class="contact-field" type="text" name="name" id="name" placeholder="What\'s your name?" />\n\t\t</label>\n\t</p>\n\t<p>\n\t\t<label for="e-mail">E-mail\n\t\t\t<input class="contact-field" type="email" name="email" id="email" placeholder="An email to answer you">\n\t\t</label>\n\t</p>\n\t<p>\n\t\t<label for="message">Message\n\t\t\t<textarea class="message" id="message" name="message" rows="5" placeholder="Your message"></textarea>\n\t\t</label>\n\t</p>\n\t<p>\n\t\t<input class="submit" type="submit" value="Submit" />\n\t</p>\n</form>';
-  $contactWays.append(contactForm);
-
   // smooth scrolling
   {
     let $btns = $('.nav-link');
@@ -216,5 +186,10 @@ function debounce(func, wait, immediate) {
     hammer.add( new Hammer.Swipe({direction: Hammer.DIRECTION_HORIZONTAL, threshold: 10}));
     hammer.on('swiperight', panelToTheRight);
     hammer.on('swipeleft', panelToTheLeft);
+  }
+
+  // add email address
+  {
+     $('#email').attr('href', 'mailto:juanriqgon@gmail.com');
   }
 })();
