b616094c67661c59147de3c4a8d7f7a3a732a3ac
soulchainer <soulchainer@gmail.com>
2016-03-14 00:44:23 +0100

    Start using JSDoc for documenting JS in Tic Tac Toe

--- FrontEnd/BuildTicTacToeGame/src/js/app.js
+++ FrontEnd/BuildTicTacToeGame/src/js/app.js
@@ -1,9 +1,22 @@
 (function(){
+    /** @constant {String} EMPTY - Represent an EMPTY square in the board */
     const EMPTY = '*';
+    /** @constant {String} X - The X token for the board*/
     const X = 'X';
+    /** @constant {String} O - The O token for the board */
     const O = 'O';
+    /**
+     * @constant {Number} TOP - The maximum value to be used by the search
+     * tree algorithm.
+     */
     const TOP = 100;
 
+    /**
+     * Answer if the row isn't empty and all their elements are equal.
+     * @param {String[]} row - Array of tokens of one winning line.
+     * @returns {Boolean} True if all line elements are equal and not the
+     * empty token, false otherwise.
+     */
     function threeInARow(row) { // return true if all row elements are equal
         let token = row[0];
         if (token !== EMPTY) {
@@ -11,12 +24,15 @@
         }
     }
 
+    /** Class representing a board. */
     class Board {
-        constructor() { // initialize the board
+        /** Create a board. */
+        constructor() {
             this.squares = Array(9).fill(EMPTY);
         }
 
-        draw() { // draw the game board
+        /** Draw the game board. */
+        draw() {
             let board = '';
             for (let [index, value] of this.squares.entries()) {
                 board += (index % 3)? value: `\n${value}`;
@@ -24,16 +40,35 @@
             console.log(board);
         }
 
-        makeMove(move, player) { // make a move
+        /**
+         * Place the token of a player in the board.
+         * @param {Number} move - The position where the token will be placed.
+         * @param {String} player - The token of the player.
+         */
+        makeMove(move, player) {
             this.squares[move] = player;
         }
 
-        undoMove(move) { // undo a move
+        /**
+         * Remove the player token in the given position of the board.
+         * @param {Number} move - The position of the token to be erased.
+         */
+        undoMove(move) {
             this.makeMove(move, EMPTY);
         }
     }
 
+    /** Class representing a Tic Tac Toe game */
     class TicTacToe {
+        /**
+         * Create a Tic Tac Toe game.
+         * @property {Board} board - The board where the game is played.
+         * @property {Object.<String, String>} - Pairs each token with their
+         * opposite, for easy switching between player tokens.
+         * @property {Number} turn - The actual turn of the game.
+         * @property {Array[]} lines - Array of winning lines in a Tic Tac Toe
+         * game.
+         */
         constructor() {
             this.board = new Board();
             this.rival = {'O': 'X', 'X': 'O'};
@@ -43,7 +78,11 @@
                         [0,4,8],[2,4,6]];          // diagonal
         }
 
-        getValidMoves() { // return valid moves still available (empty) in board
+        /**
+         * Get squares still available (empty) in the board.
+         * @returns {Number[]} List of empty squares in the board.
+         */
+        getValidMoves() {
             let squares = this.board.squares;
             let emptySquare = function(element, index) {
                 return squares[index] === EMPTY;
@@ -51,11 +90,19 @@
             return [...squares.keys()].filter(emptySquare.bind(this));
         }
 
-        isGameOver() { // return true if game ended (someone won or board full)
+        /**
+         * Answer if the game has come to an end.
+         * @returns {Boolean} True or false if game has come to an end or not.
+         */
+        isGameOver() {
             return this.getWinner() || !this.getValidMoves().length;
         }
 
-        getWinner() { // return the winner: X, O  or null (tie)
+        /**
+         * Get the token of the winner player.
+         * @returns {?String} The token of the actual winner. null means a tie.
+         */
+        getWinner() {
             for (let line of this.lines) {
                 let row = line.map(element => this.board.squares[element]);
                 if (threeInARow(row)) {
@@ -65,12 +112,22 @@
             return null;
         }
 
+        /**
+         * Evaluate (score) the state of the player at the end of a game.
+         * The possible states are: «Win» (a positive number),
+         * «Lose» (a negative) or «Tie» (zero).
+         * Take the number of moves done (depth) into account. Not taking that
+         * into account leads to a CPU playing awfully: a win in 4 moves is
+         * more valuable that one achieved in 6.
+         * @param {String} player - The token of the player which state is
+         * being evaluated.
+         * @param {Number} depth - The number of moves done to reach the actual
+         * state of the game.
+         *
+         * @returns {Number} The score of the actual state from the player
+         * perspective.
+         */
         evalFinalState(player, depth) {
-        // eval state at the end of the game for player
-        // 0 is a tie, positive values, winner states and negative, loser ones
-        // A status value depends of the depth when you reach it: a winner
-        // status reached sooner is better than one reached later
-        // If we don't have depth into account, cpu plays awfully
             let winner = this.getWinner();
             if (winner === player) {
                 return TOP - depth;
@@ -80,10 +137,15 @@
             return 0;
         }
 
+        /**
+         * Check if the game can continue or not and act according to that.
+         * If game can continue, run the next player turn.
+         * If not, draw the final status of the game
+         * @param {String} player - The token of the actual player.
+         * @param {Function} playerFunction - The function to execute if the
+         * game should continue.
+         */
         continueGame(player, playerFunction) {
-        // check if the game continues or not
-        // if game can continue, run the next player turn
-        // if not, draw the final status of the game
             if (this.isGameOver()) {
                 this.board.draw();
                 let winner = this.getWinner();
@@ -93,9 +155,12 @@
             }
         }
 
+        /**
+         * Implement search of the best move to execute next in the game.
+         * Use MiniMax algorithm enhanced with alpha-beta pruning.
+         * @returns {Number} bestMove - Best move to do next to play perfectly.
+         */
         minimax(player, depth) {
-        // return the best move to do from the actual game state
-        // use alpha-beta pruning algorithm, an improvement of minimax one
             let bestMove,
                 currentMax,
                 max = -TOP;
@@ -110,11 +175,22 @@
             return bestMove;
         }
 
+        /**
+         * Evaluate the given position and get the best result the player could
+         * achieve from that.
+         * @param {Number} move - The token position being evaluated.
+         * @param {Number} depth - The number of moves done to reach the
+         * actual state of the game.
+         * @param {String} player - The token of the player movement being
+         * evaluated.
+         * @param {Number} α - Best result possible till the moment for the
+         * player.
+         * @param {Number} β - Best result possible till the moment for the
+         * opponent.
+         * @returns {Number} Best situation the player could achieve from the
+         * given position.
+         */
         maxValue(move, depth, player, α, β) {
-        // maximizer function
-        // return a integer with the calculated value of the best
-        // situation that «move» could lead the player
-        // Bigger value means more chance to win
             try {
                 this.board.makeMove(move, player);
                 if (this.isGameOver()) {
@@ -135,12 +211,22 @@
             }
         }
 
+        /**
+         * Evaluate the given position and get the worst result for the player
+         * opponent (the best for the player).
+         * @param {Number} move - The token position being evaluated.
+         * @param {Number} depth - The number of moves done to reach the
+         * actual state of the game.
+         * @param {String} player - The token of the player movement being
+         * evaluated.
+         * @param {Number} α - Best result possible till the moment for the
+         * player.
+         * @param {Number} β - Best result possible till the moment for the
+         * opponent.
+         * @returns {Number} Worst situation the opponent player could achieve
+         * from the given position.
+         */
         minValue(move, depth, player, α, β) {
-        // minimizer function
-        // return a integer with the calculated value of the best
-        // situation that «move» could lead to rival
-        // Smaller value means more chance to win for the rival
-        // (and more chance to lose to the player)
             try {
                 this.board.makeMove(move, player);
                 if (this.isGameOver()) {
@@ -161,7 +247,11 @@
             }
         }
 
-        cpuPlayer(player) { // run cpu player turn
+        /**
+         * Run cpu player turn.
+         * @param {String} player - Token for the actual player.
+         */
+        cpuPlayer(player) {
             console.time('cpu move');
             this.board.draw();
             let move;
@@ -177,7 +267,11 @@
             this.continueGame(player, this.humanPlayer);
         }
 
-        humanPlayer(player) { // run human player turn
+        /**
+         * Run human player turn.
+         * @param {String} player - Token for the actual player.
+         */
+        humanPlayer(player) {
             this.turn++;
             console.log(`Turn ${this.turn}`);
             this.board.draw();
@@ -200,7 +294,12 @@
             this.continueGame(player, this.cpuPlayer);
         }
 
-        start(playerOne='cpu', token='X') { // start the game
+        /**
+         * Start the Tic Tac Toe game.
+         * @param {String} [playerOne='cpu'] - Player one (starts the game).
+         * @param {String} [token='X'] - Token of the player one.
+         */
+        start(playerOne='cpu', token='X') {
             if (playerOne === 'cpu') {
                 this.cpuPlayer(token);
             } else {
@@ -209,7 +308,9 @@
         }
     }
 
-
+    /**
+     * Create and launch a Tic Tac Toe game.
+     */
     function game(){
         let ttt = new TicTacToe();
         ttt.start();
